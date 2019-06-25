88149f04a81f60bbebbda892c0e42e3ec666224b
chandrakanthbalaraman <chandrakanth.b.1990@gmail.com>
2019-06-25 08:05:22 +0530

    GENERATED CODE

--- src/app/common/_services/common/project.service.ts
+++ src/app/common/_services/common/project.service.ts
@@ -1,20 +1,97 @@
 import { Injectable } from '@angular/core';
+import { generateProject } from '@assets/wizard/ngx/config-project/generate';
 var mkdirp = require('mkdirp');
+var nunjucks = require('nunjucks');
+var async = require('async');
+var fs = require('fs');
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProjectService {
-    makeDir(dirData) {
+    generateProject(dirData) {
+        generateProject.appInfo[0].name = 'app';
         return new Promise((resolve, reject) => {
-            mkdirp(dirData, (err, resp) => {
-                if (err) {
-                    reject(err);
+            async.forEach(generateProject.appInfo,function(item,loopCb){
+                if(this.makeDir(dirData,item,reqObj)==undefined){
+                    loopCb();
                 }
-                else {
+            },function(err,resp){
+                if(err){
+                    reject(err);
+                }else{
                     resolve(resp);
                 }
-            });
+            })
         })
+    }
+
+
+    makeDir(dir,data,reqObj){
+        if (typeof data == typeof {} && Array.isArray(data) == true) {
+            for(let obj of data){
+                this.makeDir(dir, obj,reqObj)   
+            }
+            
+        }else if(typeof data == typeof {} && Array.isArray(data) == false){
+            this.createIt(dir,data,reqObj,function(err,resp){
+                if(resp.hasOwnProperty('dir')){
+                    this.makeDir(dir+'/'+resp.name, resp.dir,reqObj)   
+                }
+            })
+        }
+        
+    }
+
+    createIt(dir,obj,reqObj,fbCb){
+        var dirData = dir+'/'+obj.name;
+        console.log("dirData",dirData);
+        async.waterfall([
+            function(callback){
+                mkdirp(dirData, function (err,resp) {
+                    if (err) return callback(err);
+                    else{
+                        callback(null,obj)    
+                    }
+                });
+            },
+            function(obj,callback){
+                if(obj.hasOwnProperty('file')){
+                    nunjucks.configure({ autoescape: true });
+                    async.eachSeries(obj.file, function iterator(item, loopCb) {
+                        if(item.name){
+                            if(item.hasOwnProperty('type')){
+                                let content = nunjucks.render(this.configData[item.type]['template'],reqObj);
+                                fs.writeFile(dirData+"/"+item.name, content, function(err,resp){
+                                    loopCb();
+                                    
+                                });
+                            }else{
+                                let content = ''
+                                fs.writeFile(dirData+"/"+item.name, content, function(err,resp){
+                                    loopCb();
+                                });
+                            }
+                        }else{
+                            loopCb();
+                        }
+                        
+                      }, function(err,resp) {
+                        if(err){
+                            return callback(err);
+                        }else{
+                            return callback(null,obj);
+                        }
+                      });
+                }else{
+                    callback(null,obj);
+                }
+            }
+        ], function (err, result) {
+            fbCb(err,result);
+        });
+        
+        
+        
     }
 }
